19779d7d1df0f0b65e07023bb6ff8def7492aa8d
lory3396 <lory3396@gmail.com>
2025-03-24 21:23:56 +0100

    Update app.py

--- poker_assistant_builder:/app.py
+++ poker_assistant_builder:/app.py
@@ -182,4 +182,5 @@ def index():
 if __name__ == "__main__":
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
